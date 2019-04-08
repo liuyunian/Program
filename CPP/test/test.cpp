8921452cf74def58b00f77041c800f2a228d74ef
liuyunian <sdjn_lyn@163.com>
2019-04-08 18:12:31 +0800

    2019-04-08-18

--- CPP/test/test.cpp
+++ CPP/test/test.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
 #include <vector>
 
+class A{
+private:
+    int m_value1;
+    int m_value2;
+
+public:
+    A(int v1 = 0, int v2 = 0) : m_value1(v1), m_value2(v2){
+        std::cout << "执行了构造函数A(int v1 = 0, int v2 = 0)" << std::endl;
+        std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
+    }
+
+    A(const A & a) : m_value1(a.m_value1), m_value2(a.m_value2){
+        std::cout << "执行了拷贝构造函数" << std::endl;
+    }
+
+    ~A(){
+        std::cout << "执行了析构函数" << std::endl;
+    }
+
+    A & operator=(const A & a){
+        m_value1 = a.m_value1;
+        m_value2 = a.m_value2;
+
+        std::cout << "执行了重载赋值运算符" << std::endl;
+        std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
+    }
+
+    // void add(A & a){
+    //     m_value1 += a.m_value1;
+    //     m_value2 += a.m_value2;
+    //     std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
+
+    //     a.m_value1 = 1000;
+    // }
+};
+
 int main(){
-    std::vector<int> * v_ptr = new std::vector<int>();
-    for(int i = 0; i < 10; i++){
-        v_ptr->push_back(i);
-    }
-    
-    for(auto item : *v_ptr){
-        std::cout << item << std::endl;
-    }
+    // A a1;
+    // a1 = 10;
+    A a1 = 10;
 
     return 0;
 }
