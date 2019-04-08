31f573b234d7511499ce6b8e6fc7bcc4ceba219b
liuyunian <sdjn_lyn@163.com>
2019-04-08 21:28:00 +0800

    2019-04-08-21

--- CPP/test/test.cpp
+++ CPP/test/test.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 
 class A{
-private:
+public:
     int m_value1;
-    int m_value2;
+    int m_value2 = 0;
 
 public:
     A(int v1 = 0, int v2 = 0) : m_value1(v1), m_value2(v2){
@@ -12,12 +12,19 @@ public:
         std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
     }
 
+    // A(int v1) : m_value1(v1){
+    //     std::cout << "执行了构造函数A(int)" << std::endl;
+    //     std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
+    // }
+
     A(const A & a) : m_value1(a.m_value1), m_value2(a.m_value2){
         std::cout << "执行了拷贝构造函数" << std::endl;
+        std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
     }
 
     ~A(){
         std::cout << "执行了析构函数" << std::endl;
+        std::cout << "(" << m_value1 << ", " << m_value2 << ")" << std::endl;
     }
 
     A & operator=(const A & a){
@@ -37,10 +44,18 @@ public:
     // }
 };
 
+A Double(A & a){
+    A a_tmp;
+    a_tmp.m_value1 = a.m_value1*2;
+    a_tmp.m_value2 = a.m_value2*2;
+
+    return a_tmp;    
+}
+
 int main(){
-    // A a1;
-    // a1 = 10;
-    A a1 = 10;
+    A a(1, 2);
+    A a2 = Double(a);
+    std::cout << "main function" << std::endl;
 
     return 0;
 }
