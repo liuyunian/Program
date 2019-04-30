98862cf8eb7e0e78b13abb40a139f695bcb2d35d
liuyunian <sdjn_lyn@163.com>
2019-04-30 11:39:30 +0800

    2019-04-29-12

--- CPP/test/test.cpp
+++ CPP/test/test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <boost/type_index.hpp>
 
-void func(int data){
+// void func(int data){
+//     std::cout << data << std::endl;
+// }
+
+int func(int data){
     std::cout << data << std::endl;
 }
 
@@ -34,10 +38,34 @@ int main(){
     // auto m = str;
     // auto & n = str;
 
-    auto f = func;
+    // auto f = func;
+
+    // int i = 10;
+    // decltype(i) data; // data = int
+
+    // const int j = 10;
+    // decltype(j) data = 0;
+
+    // int &j = i;
+    // decltype(j) data = j;
+
+    // const int &j = i;
+    // decltype(j) data = i;
+
+    // int * p = &i;
+    // decltype(p) data;
+
+    // int && j = std::move(i);
+    // decltype(j) data = 10;
+
+    // int i = 10;
+    // int * p = &i;
+    // decltype(*p) data = i;
+
+    // decltype(func) data;
 
     using boost::typeindex::type_id_with_cvr;
-    std::cout << "f = " << type_id_with_cvr<decltype(f)>().pretty_name() << std::endl;
+    std::cout << "data = " << type_id_with_cvr<decltype(data)>().pretty_name() << std::endl;
 
     return 0;
 }
