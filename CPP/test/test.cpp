8550e112dbfa8a654088b683489ae1e4d9294a6a
liuyunian <sdjn_lyn@163.com>
2019-05-02 12:35:15 +0800

    2019-05-02-12

--- CPP/test/test.cpp
+++ CPP/test/test.cpp
@@ -1,71 +1,12 @@
 #include <iostream>
-#include <boost/type_index.hpp>
-
-// void func(int data){
-//     std::cout << data << std::endl;
-// }
-
-int func(int data){
-    std::cout << data << std::endl;
-}
+#include <vector>
 
 int main(){
-    // auto i = 3;
-    // const auto j = 4;
-    // int i = 9;
-    // const int & j = i;
-    // auto x = j;
-    // int data  = 10;
-    // auto & x = data;
-    // const auto & y = data; 
-    // auto & i = y;
-
-    // auto * x = &data;
-    // auto y = &data;
-
-    // auto p = new int(10);
-
-    // auto && ref = 10;
-    // int data = 1;
-    // auto && ref2 = data;
-    // auto && ref3 = std::move(data);
-
-    // int data[] = {1, 2};
-    // auto i = data;
-    // auto & j = data;
-
-    // const char str[] = "hello";
-    // auto m = str;
-    // auto & n = str;
-
-    // auto f = func;
-
-    // int i = 10;
-    // decltype(i) data; // data = int
-
-    // const int j = 10;
-    // decltype(j) data = 0;
-
-    // int &j = i;
-    // decltype(j) data = j;
-
-    // const int &j = i;
-    // decltype(j) data = i;
-
-    // int * p = &i;
-    // decltype(p) data;
-
-    // int && j = std::move(i);
-    // decltype(j) data = 10;
-
-    // int i = 10;
-    // int * p = &i;
-    // decltype(*p) data = i;
-
-    // decltype(func) data;
-
-    using boost::typeindex::type_id_with_cvr;
-    std::cout << "data = " << type_id_with_cvr<decltype(data)>().pretty_name() << std::endl;
+    std::vector<int> v{0, 1, 2};
+    for(std::vector<int>::iterator iter = v.begin(); iter != v.end(); ++ iter){
+        v.push_back(3);
+        std::cout << *iter << std::endl;
+    }
 
     return 0;
 }
